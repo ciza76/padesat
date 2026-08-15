d974eb2c31242f7ab99128788df502a8d08fad78
xzcizek00 <zdenek.cizek@post.cz>
2026-08-15 22:36:06 +0200

    init3

--- root/server.py
+++ root/server.py
@@ -1,16 +1,5 @@
-import psycopg2
-from flask import Flask, jsonify, abort, render_template, redirect, url_for, send_file, request, flash, Response, send_from_directory, session
-import random
-import string
+from flask import Flask, render_template
 import os
-import io
-from collections import Counter
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user, login_required, logout_user, UserMixin, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
-from dotenv import load_dotenv
-import math
-from typing import Dict, Union, List
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '01e8b1c0bb4c2340d8d16db0d1ed43adf917858700e48391b06cfc5292ce99b1'
